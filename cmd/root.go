6682b7222c94d40a725211ec4f5e01b324c8cade
Flávio Antonio Severino <fasvb6@gmail.com>
2026-02-16 16:50:44 -0300

    feat:add tail functionality

--- cmd/root.go
+++ cmd/root.go
@@ -8,55 +8,50 @@ import (
 	"logagg/internal/reader"
 	"os"
 	"os/signal"
-	"sync"
 
 	"github.com/spf13/cobra"
 )
 
 var files []string
 var filterParam string
+var tail bool
 
 var rootCmd = &cobra.Command{
 	Use:   "logagg",
 	Short: "Monitorador de logs",
 	Run: func(cmd *cobra.Command, args []string) {
 		ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
-		var wg sync.WaitGroup
 		defer cancel()
 		channels := make([]<-chan string, 0, len(files))
-		fmt.Println(files)
+
 		for _, f := range files {
 
 			if err := reader.ValidateFile(f); err != nil {
 				fmt.Println("Erro: ", err)
 				continue
 			}
-			ch := reader.ReadLines(ctx, f)
+
+			ch := reader.ReadLines(ctx, f, tail)
 			channels = append(channels, ch)
 
 		}
 
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
+		lines := aggregator.Aggregate(ctx, channels...)
+		result := filter.Filter(lines, filterParam)
 
-			lines := aggregator.Aggregate(ctx, channels...)
-			result := filter.Filter(lines, filterParam)
+		for l := range result {
+			fmt.Println(l)
+		}
 
-			for l := range result {
-				fmt.Println(l)
-			}
-
-		}()
-		wg.Wait()
-		fmt.Println("Logs processados")
 	},
 }
 
 func init() {
 
 	rootCmd.Flags().StringSliceVarP(&files, "files", "f", []string{}, "Arquivos para monitorar")
-	rootCmd.Flags().StringVarP(&filterParam, "filter", "t", "", "Filtar o retorno do log por palavra")
+	rootCmd.Flags().StringVarP(&filterParam, "filter", "F", "", "Filtar o retorno do log por palavra")
+	rootCmd.Flags().BoolVarP(&tail, "tail", "t", false, "Aguarda novas linhas no arquivo de log")
+
 }
 
 func Execute() {
--- internal/reader/reader.go
+++ internal/reader/reader.go
@@ -10,7 +10,7 @@ import (
 	"time"
 )
 
-func ReadLines(ctx context.Context, file string) <-chan string {
+func ReadLines(ctx context.Context, file string, tail bool) <-chan string {
 	out := make(chan string)
 
 	go func() {
@@ -20,16 +20,31 @@ func ReadLines(ctx context.Context, file string) <-chan string {
 			log.Fatal(err)
 		}
 		defer f.Close()
+
 		scanner := bufio.NewScanner(f)
-		for scanner.Scan() {
-			time.Sleep(time.Millisecond * 10)
-			select {
-			case out <- fmt.Sprintf("[%s] - %s", filepath.Base(file), scanner.Text()):
-			case <-ctx.Done():
-				fmt.Println("Sistema encerrado")
+
+		for {
+			for scanner.Scan() {
+				select {
+				case out <- fmt.Sprintf("[%s] - %s", filepath.Base(file), scanner.Text()):
+				case <-ctx.Done():
+					return
+				}
+			}
+
+			if !tail {
 				return
+			}
+
+			select {
+			case <-ctx.Done():
+				return
+			case <-time.After(500 * time.Millisecond):
+				scanner = bufio.NewScanner(f)
 			}
 		}
 	}()
+
 	return out
+
 }
